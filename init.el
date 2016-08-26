a3787af1e51254832294812021b4a56859b0172a
Davor Babić <davor@davor.se>
2016-08-26 21:43:57 +0200

    Rewrites all files, from the ground up.

--- init.el
+++ init.el
@@ -1,29 +1,36 @@
-;;; init.el --- Where all the magic begins
+;;; init.el -- and then there was light
 ;;
-;; Part of the Emacs Starter Kit
-;;
-;; This is the first thing to get loaded.
-;;
+;; this is the first thing to get loaded
+(require 'package)
 
-;; load Org-mode from source when the ORG_HOME environment variable is set
-(when (getenv "ORG_HOME")
-  (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
-    (when (file-directory-p org-lisp-dir)
-      (add-to-list 'load-path org-lisp-dir)
-      (require 'org))))
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
-;; load the starter kit from the `after-init-hook' so all packages are loaded
-(add-hook 'after-init-hook
- `(lambda ()
-    ;; remember this directory
-    (setq starter-kit-dir
-          ,(file-name-directory (or load-file-name (buffer-file-name))))
-    ;; only load org-mode later if we didn't load it just now
-    ,(unless (and (getenv "ORG_HOME")
-                  (file-directory-p (expand-file-name "lisp"
-                                                      (getenv "ORG_HOME"))))
-       '(require 'org))
-    ;; load up the starter kit
-    (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
+(require 'org)
+(require 'ob-tangle)
+(org-babel-load-file "~/.emacs.d/emacs.org")
+(org-babel-load-file "~/.emacs.d/packages.org")
+(org-babel-load-file "~/.emacs.d/keybindings.org")
+(org-babel-load-file "~/.emacs.d/programming.org")
+(org-babel-load-file "~/.emacs.d/look.org")
+(org-babel-load-file "~/.emacs.d/git.org")
+(org-babel-load-file "~/.emacs.d/lisp.org")
+(org-babel-load-file "~/.emacs.d/misc.org")
 
-;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (paredit dracula-theme magit yasnippet expand-region yaml-mode wc-mode undo-tree markdown-mode highlight-symbol highlight-sexp darkroom ack ace-jump-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
