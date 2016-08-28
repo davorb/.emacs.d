79347e014d2f434823d16989ba5a67c17c810c4b
Davor Babić <davor@davor.se>
2016-08-28 10:42:27 +0200

    adds helm-mode

--- init.el
+++ init.el
@@ -11,8 +11,8 @@
 
 (require 'org)
 (require 'ob-tangle)
-(org-babel-load-file "~/.emacs.d/emacs.org")
 (org-babel-load-file "~/.emacs.d/packages.org")
+(org-babel-load-file "~/.emacs.d/emacs.org")
 (org-babel-load-file "~/.emacs.d/functions.org")
 (org-babel-load-file "~/.emacs.d/misc.org")
 (org-babel-load-file "~/.emacs.d/keybindings.org")
@@ -21,10 +21,6 @@
 (load-host-specific-conf)
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
     (enh-ruby-mode auto-complete column-marker paredit dracula-theme magit yasnippet expand-region yaml-mode wc-mode undo-tree markdown-mode highlight-symbol highlight-sexp darkroom ack ace-jump-mode))))
