320ec661f050cbfefe10e52dc0f3b2a65b85b463
Davor Babić <davor@davor.se>
2016-08-31 10:49:28 +0200

    removes comment from init.el

--- init.el
+++ init.el
@@ -31,8 +31,4 @@
    (quote
     ("427fed191e7a766152e59ef0e2904283f436dbbe259b9ccc04989f3acde50a55" default))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
