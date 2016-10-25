352daf5c19ea6b80b0fc114756a9122777cae515
Davor Babić <davor@davor.se>
2016-10-25 15:00:51 +0200

    auto added stuff

--- init.el
+++ init.el
@@ -29,6 +29,14 @@
  '(custom-enabled-themes (quote (dracula)))
  '(custom-safe-themes
    (quote
-    ("427fed191e7a766152e59ef0e2904283f436dbbe259b9ccc04989f3acde50a55" default))))
+    ("e56ee322c8907feab796a1fb808ceadaab5caba5494a50ee83a13091d5b1a10c" "427fed191e7a766152e59ef0e2904283f436dbbe259b9ccc04989f3acde50a55" default)))
+ '(package-selected-packages
+   (quote
+    (js2-mode nodejs-repl dracula-theme gnu-apl-mode tern-auto-complete tern multiple-cursors intero yasnippet yaml-mode wc-mode use-package undo-tree powerline paredit material-theme markdown-mode magit highlight-symbol highlight-sexp helm flymake-easy expand-region enh-ruby-mode darkroom column-marker auto-complete ack ace-jump-mode)))
+ '(safe-local-variable-values (quote ((org-confirm-babel-evaluate)))))
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
