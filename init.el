8822ef253b93c653a13316fd39c36601d7b69fa7
Davor Babić <davor@davor.se>
2016-11-04 16:53:24 +0100

    big update with use-package and rust language support

--- init.el
+++ init.el
@@ -14,21 +14,21 @@
 	  (require 'org)
 	  (require 'ob-tangle)
 	  (org-babel-load-file "~/.emacs.d/packages.org")
-	  (org-babel-load-file "~/.emacs.d/emacs.org")
-	  (org-babel-load-file "~/.emacs.d/functions.org")
-	  (org-babel-load-file "~/.emacs.d/misc.org")
-	  (org-babel-load-file "~/.emacs.d/keybindings.org")
-	  (org-babel-load-file "~/.emacs.d/programming.org")
-	  (org-babel-load-file "~/.emacs.d/look.org")
-	  (load-host-specific-conf)))
+	  (org-babel-load-file "~/.emacs.d/emacs.org")))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (dracula)))
  '(custom-safe-themes
    (quote
-    ("427fed191e7a766152e59ef0e2904283f436dbbe259b9ccc04989f3acde50a55" default))))
+    ("427fed191e7a766152e59ef0e2904283f436dbbe259b9ccc04989f3acde50a55" default)))
+ '(package-selected-packages
+   (quote
+    (rustfmt cargo rust-mode yasnippet yaml-mode wc-mode use-package undo-tree tern-auto-complete powerline paredit nodejs-repl nix-mode multiple-cursors material-theme markdown-mode magit intero hydandata-light-theme highlight-symbol highlight-sexp helm gnu-apl-mode github-theme expand-region enh-ruby-mode dracula-theme darkroom column-marker circe atomic-chrome ack ace-jump-mode))))
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
