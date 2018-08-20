db292d58d8d0e25035f2834dc0ba2fb348911633
Davor Babic <davorb@trinity.local>
2018-08-20 12:50:09 +0200

    add local changes for trintiy computer

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rustfmt cargo rust-mode yasnippet yaml-mode writeroom-mode wc-mode use-package undo-tree tide tern-auto-complete stylus-mode powerline paredit nodejs-repl nix-mode multiple-cursors material-theme markdown-mode magit js2-mode intero hydandata-light-theme highlight-symbol highlight-sexp helm gnu-apl-mode flymake-jshint expand-region enh-ruby-mode dracula-theme darkroom column-marker ack ace-jump-mode))))
+    (projectile visual-fill-column nasm-mode go-mode sass-mode web-mode typescript-mode geiser auto-complete org-clock-today evil swiper ivy rustfmt cargo rust-mode yasnippet yaml-mode writeroom-mode wc-mode use-package undo-tree tide tern-auto-complete stylus-mode powerline paredit nodejs-repl nix-mode multiple-cursors material-theme markdown-mode magit js2-mode intero hydandata-light-theme highlight-symbol highlight-sexp helm gnu-apl-mode flymake-jshint expand-region enh-ruby-mode dracula-theme darkroom column-marker ack ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
